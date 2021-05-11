206638d19611549fc670f6516ae86dbfe47cf139
DemitreB <antonio.beltran@stu.boone.kyschools.us>
2021-05-11 08:40:22 -0400

    added pics

--- Road.java
+++ Road.java
@@ -1,19 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Back here.
+ * Write a description of class Road here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Back extends World
+public class Road extends World
 {
 
     /**
-     * Constructor for objects of class Back.
+     * Constructor for objects of class Road.
      * 
      */
-    public Back()
+    public Road()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
